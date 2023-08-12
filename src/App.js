faf5b1d61e8f6e693f3e6e225b0b701a0531c60a
Comfy <kenwadder27@gmail.com>
2023-08-11 22:12:15 -0500

    created sliding background, need to reduce opacity, need to make it more continuous

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import HeaderNavbar from './components/Navbar.js';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import SectionOneBg from './components/Sectionone.js';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
           </Routes>
         </HeaderNavbar>
       </Router>
+      <SectionOneBg/>
+
     </div>
   );
 }
