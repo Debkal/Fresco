97eb6b4360cf608cd40989f2ff4bc4aed52baa64
Comfy <kenwadder27@gmail.com>
2023-08-18 01:24:08 -0500

    optimized header, and section one, might need some tweaks

--- src/components/Videobg.js
+++ src/components/Videobg.js
@@ -7,27 +7,50 @@ import VideoPlayer from './Videoplayer';
 import ReactPlayer from 'react-player';
 
 function VideoBg() {
-
+    const boxStyle = {
+        '@media (min-width:600px)':{
+            width: '80%'
+        }
+    }
     return (
         <>
         
             <Box 
             maxWidth='md'
             sx={{
+            top: {
+                xs:'35%',
+                sm:'30%',
+                md:'20%',
+            },
             display:'flex',
             justifyContent: 'center',
             marginTop: '2.5rem',
             position: 'absolute',
             backgroundImage: `url(${windowframe1})`,
-            minHeight:'730px',
+            backgroundSize: 'contain',
+            minHeight: {
+                xs:'378px',
+                sm:'550px',
+                md:'795px',
+            },
             maxHeight:'795px',
             maxWidth:'730px',
-            height: '100%',
-            width: '100%',
+            objectFit: 'cover',
+            height: {
+                xs:'10% !important',
+                sm:'70%'
+            },
+            width: {
+                xs:'90%',
+                sm:'100%',
+                md:'80%',
+            },
             border: 'black solid 1px',
             borderRadius: '.2em',
             zIndex: '2',
             overflow: 'visible',
+            
             }}
             >
                 <VideoPlayer></VideoPlayer>
